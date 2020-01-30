e6d9aa21f6a7e45dcf6c4b09c863a108009eccf9
Sivaprasad Tamatam <Sivaprasad.Tamatam@wdc.com>
2020-01-30 22:25:58 +0530

    Updates

---
+++ precache-manifest.8f367b5997ea43851bd66b6d0603bdf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f84f8541e07155b80ab5d83e36af34",
+    "url": "/monter-rodolex/index.html"
+  },
+  {
+    "revision": "0abea276994e444c3290",
+    "url": "/monter-rodolex/static/css/main.7768a6e2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monter-rodolex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monter-rodolex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0abea276994e444c3290",
+    "url": "/monter-rodolex/static/js/main.3db0be8e.chunk.js"
+  },
+  {
+    "revision": "ed22f51f934f17549a8b",
+    "url": "/monter-rodolex/static/js/runtime-main.9d192401.js"
+  }
+]);
